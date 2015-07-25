beccccea40cba25e30a06758e4527d4b48d2029d
Robert Michael <rmichael@ecklebe-engineering.com>
2015-07-25 10:13:30 +0200

    Test environment for helloWorld

---
+++ Hello-World/Hello-World/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World {
+    public static class Functions {
+
+        public static int getMultiplication(int var1, int var2) {
+
+            return var1 * var2;
+        }
+    }
+}
---
+++ Hello-World/HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello_World;
+
+namespace HelloWorldTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+
+            int value1 = 10;
+            int value2 = 20;
+
+            int testresult = value1 * value2;
+
+            Assert.AreEqual(testresult, Functions.getMultiplication(12, value2));
+
+        }
+    }
+}
